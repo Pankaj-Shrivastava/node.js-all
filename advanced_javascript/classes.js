984be7aa65e2be6a4c251c00a3b67e06bb7575d2
Pankaj-Shrivastava <Pankaj.Shrivastava@ibm.com>
2026-01-12 19:34:08 +0530

    js classes

---
+++ advanced_javascript/classes.js
@@ -0,0 +1,29 @@
+// Classes are syntactical sugar over js prototypes
+
+class Person {
+  constructor(fName, lName) {
+    this.firstName = fName;
+    this.lastName = lName;
+  }
+
+  sayMyName() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const classP1 = new Person("Bruce", "Wayne");
+console.log(classP1.sayMyName());
+
+class SuperHero extends Person {
+  constructor(fName, lName) {
+    super(fName, lName);
+    this.isSuperHero = true;
+  }
+
+  fightCrime() {
+    console.log("Fighting crime");
+  }
+}
+
+const batman = new SuperHero("Bruce", "Wayne");
+console.log(batman.sayMyName());
